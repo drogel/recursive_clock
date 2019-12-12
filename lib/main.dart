fc385dbfdee789605ed978e971239da76622a22c
Diego.Rogel <diego.rogel@cognizant.com>
2019-12-12 12:16:41 +0100

    inits project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:recursive_clock/helper/customizer.dart';
+import 'package:recursive_clock/recursive_clock.dart';
+
+void main() {
+  // A temporary measure until Platform supports web and TargetPlatform supports
+  // macOS.
+  if (!kIsWeb && Platform.isMacOS) {
+    // TODO(gspencergoog): Update this when TargetPlatform includes macOS.
+    // https://github.com/flutter/flutter/issues/31366
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override.
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+
+  runApp(ClockCustomizer((_) => RecursiveClock()));
+}
---
+++ lib/recursive_clock.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecursiveClock extends StatefulWidget {
+  @override
+  _RecursiveClockState createState() => _RecursiveClockState();
+}
+
+class _RecursiveClockState extends State<RecursiveClock> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
