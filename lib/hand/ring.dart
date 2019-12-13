ae1203d0a8adf2114cfd0823ca2aa52168ce3f55
diego-rogel <diegorogel3d@hotmail.com>
2019-12-13 20:39:40 +0100

    lets ring widget take style and size parameters

--- lib/hand/ring.dart
+++ lib/hand/ring.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
 
+const double _kStrokeWidth = 1;
+
 class Ring extends StatelessWidget {
+  const Ring({
+    this.color = Colors.black,
+    this.strokeWidth = _kStrokeWidth,
+  })  : assert(color != null),
+        assert(strokeWidth != null);
+
+  final Color color;
+  final double strokeWidth;
+
   @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: const _RingPainter(),
-    );
-  }
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (_, constraints) => CustomPaint(
+          painter: _RingPainter(
+            radius: (constraints.maxHeight - strokeWidth)/2,
+            color: color,
+            strokeWidth: strokeWidth,
+          ),
+        ),
+      );
 }
 
 class _RingPainter extends CustomPainter {
-  const _RingPainter();
+  const _RingPainter({
+    @required this.radius,
+    this.color = Colors.black,
+    this.strokeWidth = _kStrokeWidth,
+  })  : assert(color != null),
+        assert(radius != null),
+        assert(strokeWidth != null);
+
+  final Color color;
+  final double strokeWidth;
+  final double radius;
 
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint()
       ..style = PaintingStyle.stroke
-      ..color = Colors.black
-      ..strokeWidth = (20);
-    canvas.drawCircle(Offset.zero, 60, paint);
+      ..color = color
+      ..strokeWidth = strokeWidth;
+    canvas.drawCircle(Offset.zero, radius, paint);
   }
 
+  // TODO(drogel): review the shouldRepaint method
   @override
-  bool shouldRepaint(_RingPainter oldDelegate) => true;
+  bool shouldRepaint(_RingPainter oldDelegate) => false;
 }
